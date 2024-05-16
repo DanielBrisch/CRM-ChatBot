e273e1ede9c345e23ef9d5891f866ed83e0c3bb7
DanielBrisch <danielbrisch157@gmail.com>
2024-05-16 00:05:50 -0300

    feature: innital commit, first commit with simple code for test a IA model

---
+++ main.py
@@ -0,0 +1,45 @@
+import os 
+import gradio as gr 
+from dotenv import load_dotenv
+from llama_index.core import PromptTemplate, VectorStoreIndex, SimpleDirectoryReader
+
+load_dotenv()
+
+documents = SimpleDirectoryReader('./data').load_data()
+
+TEMPLATE_STR = (
+    "Nós fornecemos informaccoes do contexto abaixo.\n"
+    "-------------------------------------------------"
+    "{context_str}"
+    "\n-------------------------------------------------\n"
+    "Somente com base nessas informacoes, por favor responda a seguinte pergunta: {query_str}\n"
+)
+
+QA_TEMPLATE = PromptTemplate(TEMPLATE_STR)
+
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
+
+def run_code(question):
+    if question:
+        answer = query_engine.query(question)
+        sources = answer.source_nodes
+        response = ""
+        
+        response += "\n\nResposta á pergunta:\n" + str(answer) + "\n Fonte:" 
+        return response
+    else: 
+        return "Por favor, insira uma pergunta."
+
+iface = gr.Interface(
+    fn=run_code,
+    inputs="text",
+    outputs="text",
+    title="Chatbot Federal",
+    description="descricao do chatbot",
+)
+    
+
+
+iface.launch()
