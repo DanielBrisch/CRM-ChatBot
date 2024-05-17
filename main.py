10a3dec81c1292555395b3dd83d361658e18831d
daniel.cibolli <daniel.cibolli@viasoft.com.br>
2024-05-17 08:19:02 -0300

    feture: add a new data in dataset and add a answer formatter for better readability

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import os 
-import gradio as gr 
+import os
+import gradio as gr
 from dotenv import load_dotenv
 from llama_index.core import PromptTemplate, VectorStoreIndex, SimpleDirectoryReader
+from openai import OpenAI 
 
 load_dotenv()
 
 documents = SimpleDirectoryReader('./data').load_data()
 
+api_key = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 TEMPLATE_STR = (
-    "Apenas fornecemos informaccoes do contexto abaixo.\n"
-    "-------------------------------------------------"
-    "{context_str}"
-    "\n-------------------------------------------------\n"
-    "Somente com base nessas informacoes, por favor responda a seguinte pergunta: {query_str}\n"
+    "Apenas fornecemos informações do contexto abaixo.\n"
+    "-------------------------------------------------\n"
+    "{context_str}\n"
+    "-------------------------------------------------\n"
+    "Somente com base nessas informações, por favor responda a seguinte pergunta: {query_str}\n"
 )
 
 QA_TEMPLATE = PromptTemplate(TEMPLATE_STR)
@@ -21,15 +24,28 @@ index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine(text_qa_template=QA_TEMPLATE)
 
+def reformulate_text(text):
+    response = api_key.chat.completions.create(
+        model="gpt-4", 
+        messages=[
+            {"role": "system", "content": "Você é um assistente útil que serve para reformular textos e deixar mais legiveis e atraetes para os humanos"},
+            {"role": "user", "content": f"Reformule o seguinte texto: {text}"}
+        ],
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    reformulated_text = response.choices[0].message.content
+    return reformulated_text
+
 def run_code(question):
     if question:
         answer = query_engine.query(question)
-        sources = answer.source_nodes
-        response = ""
-        
-        response += "\n\nResposta á pergunta:\n" + str(answer) + "\n Fonte:" 
+        reformulated_answer = reformulate_text(str(answer))
+        response = f"{str(reformulated_answer)}"
         return response
-    else: 
+    else:
         return "Por favor, insira uma pergunta."
 
 iface = gr.Interface(
@@ -37,9 +53,7 @@ iface = gr.Interface(
     inputs="text",
     outputs="text",
     title="Chatbot CRM",
-    description="chatBot para auxiliar no uso do CRM Web",
+    description="ChatBot para auxiliar no uso do CRM Web",
 )
-    
-
 
 iface.launch()
